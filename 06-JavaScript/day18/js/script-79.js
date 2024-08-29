a0176085ff562bba4e9c7fe7b3bfeb50f4cb66ff
Alparslan Uysal <alparslan94@gmail.com>
2024-08-29 22:13:36 +0200

    29.08.2024

--- 06-JavaScript/day18/js/script-79.js
+++ 06-JavaScript/day18/js/script-79.js
@@ -1,26 +1,35 @@
 const frmLogin = document.getElementById("frmLogin");
+
 frmLogin.addEventListener("submit", (e) => {
     e.preventDefault(); // formun submit davranisini iptal eder
+
     const txtEmail = document.getElementById("txtEmail");
     const txtPassword = document.getElementById("txtPassword");
-    try {
+
+    try { /* hata olmasini bekledigimiz kisim buraya yazilir. */
         const email = txtEmail.value;
         const password = txtPassword.value;
+
         if (!email || !isEmail(email)) {
             txtEmail.focus();
-            throw new Error("Eposta adresinizi giriniz");
+            throw new Error("Eposta adresinizi giriniz"); /* throw new Error yeni hata firlatir ve kodlarin calismasi durur catch e gider */
         }
+
         if (!password) {
             txtPassword.focus();
             throw new Error("Password giriniz");
         }
-        e.target.submit();
+        
+        e.target.submit(); /* yukarida iptal ettigimiz submit davranisini burada yaptiriyoruz */
+
     } catch (err) {
         console.log(err);
         alert(err.message);
-    }s
+    }
 });
+
+
 const isEmail = (email) => { 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; /* chat gpt ile olusturuldu. Bu kaliba uyarsa true, uymazsa false dondurur */
     return emailRegex.test(email)
 }
